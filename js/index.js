2c6aac24f624ae23eea05e0050ae8fec158ea2bb
NathanYee412 <nyee@csu.fullerton.edu>
2021-08-22 19:17:49 -0700

    completed functionality of dice challenge

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+// Get a random number between 0 and 6
+function getRandomNum() {
+    
+    let randomNumber1 = Math.floor(Math.random() * 6);
+    return randomNumber1;
+
+}
+
+
+// create an array of image paths
+let picArr = ["/img/dice1.png", "/img/dice2.png", "/img/dice3.png", "/img/dice4.png", "/img/dice5.png", "/img/dice6.png"]
+
+
+// change img 
+function getRandomDice(diceNum) {
+
+    let leftNum = getRandomNum();
+    let rightNum = getRandomNum();
+
+    document.getElementById("left").src=picArr[leftNum];
+    document.getElementById("right").src=picArr[rightNum];
+
+    if(leftNum > rightNum) {
+        document.getElementById("title-text").innerHTML = "Player 1 Wins!";
+    } else {
+        document.getElementById("title-text").innerHTML = "Player 2 Wins!";
+    }
+
+}
+
+
+
+getRandomDice();
